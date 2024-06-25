60352d59beb8ebe4a4beff17bbbf328b43c9f32b
kilelx <llx.kieran@gmail.com>
2024-06-25 16:29:42 +0200

    change: match username minimum length

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -14,43 +14,45 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
-    // Add some functions
-    async session({ session }) {
-        // Keep existing and running session
-        const sessionUser = await User.findOne({
-            email: session.user.email
-        })
-
-        session.user.id = sessionUser._id.toString();
-
-        return session;
-    },
-    async signIn({ profile }) {
-
-        try {
-            await connectToDB();
-
-            // Check if a user already exists
-            const userExists = await User.findOne({
-                email: profile.email
+    callbacks: {
+        // Add some functions
+        async session({ session }) {
+            // Keep existing and running session
+            const sessionUser = await User.findOne({
+                email: session.user.email
             })
 
-            // if not, create a new user
-            if(!userExists) {
-                await User.create({
-                    email: profile.email,
-                    // Formatting the username: delete white spaces
-                    username: profile.name.replace(" ", "").toLowerCase(),
-                    image: profile.picture
+            session.user.id = sessionUser._id.toString();
+
+            return session;
+        },
+        async signIn({ profile }) {
+
+            try {
+                await connectToDB();
+
+                // Check if a user already exists
+                const userExists = await User.findOne({
+                    email: profile.email
                 })
+
+                // if not, create a new user
+                if (!userExists) {
+                    await User.create({
+                        email: profile.email,
+                        // Formatting the username: delete white spaces
+                        username: profile.name.replace(" ", "").toLowerCase(),
+                        image: profile.picture
+                    })
+                }
+
+                return true;
+            } catch (err) {
+                console.log(err);
+                return false;
             }
-    
-            return true;
-        } catch (err) {
-            console.log(err);
-            return false;
         }
     }
 })
 
-export { handler as GET, handler as POST}
+export { handler as GET, handler as POST }
